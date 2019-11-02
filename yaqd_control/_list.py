771ff395e61a17200b79e3e2b861355516cfc0ba
Blaise Thompson <blaise@untzag.com>
2019-11-02 18:00:58 -0500

    pretty table print

--- yaqd_control/_list.py
+++ yaqd_control/_list.py
@@ -2,15 +2,13 @@ __all__ = [list]
 
 
 from ._cache import read_daemon_cache
+import prettytable
 
 
 def list():
-    print("host           port      kind      name          ")
-    print("-------------------------------------------------")
+    out = prettytable.PrettyTable()
+    out.field_names = ["host", "port", "kind", "name"]
+    out.align = "l"
     for daemon in read_daemon_cache():
-        out = ""
-        out += f"{daemon.host}".ljust(15)
-        out += f"{daemon.port}".ljust(10)
-        out += f"{daemon.kind}".ljust(10)
-        out += f"{daemon.name}".ljust(10)
-        print(out)
+        out.add_row([daemon.host, daemon.port, daemon.kind, daemon.name])
+    print(out)
