511477c2c41cd82259f68fe7a50a479b6b37cd1e
Kyle Sunden <sunden@wisc.edu>
2019-11-14 10:27:01 -0600

    Add py3.8 to classifiers

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering",
     ],
 )
